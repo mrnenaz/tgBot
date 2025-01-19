954e51d0de14a7dc3c153696207a3c972b3dbf0f
menenaz <ya.mrnenaz@yandex.ru>
2025-01-19 21:34:37 +0300

    тесты не получились

--- src/scenes/getGiveawaysInfo.ts
+++ src/scenes/getGiveawaysInfo.ts
@@ -10,6 +10,7 @@ import {
 import { BOT_EVENT_NAMES, CMD_TEXT, SCENE_NAMES } from "../constants";
 import { Markup, Scenes } from "telegraf";
 import { getRandomInt } from "../db/utils";
+import { parse } from "path";
 
 export const getGiveawaysInfo: any = new Scenes.BaseScene(
   SCENE_NAMES.GET_GIVEAWAYS_INFO
@@ -19,12 +20,12 @@ getGiveawaysInfo.enter(async (ctx: any) => {
   await ctx.reply(
     "Какие конкурсы показать?",
     Markup.inlineKeyboard([
-      [
-        Markup.button.callback(
-          CMD_TEXT.giveawaysTexts.all,
-          BOT_EVENT_NAMES.giveaways.all
-        ),
-      ],
+      // [
+      //   Markup.button.callback(
+      //     `(Не работает) ${CMD_TEXT.giveawaysTexts.all}`,
+      //     BOT_EVENT_NAMES.giveaways.all
+      //   ),
+      // ],
       [
         Markup.button.callback(
           CMD_TEXT.giveawaysTexts.active,
@@ -41,8 +42,6 @@ getGiveawaysInfo.enter(async (ctx: any) => {
 });
 
 getGiveawaysInfo.action(BOT_EVENT_NAMES.giveaways.active, async (ctx: any) => {
-  // ctx.scene.state.giveaways = "all";
-  console.log(BOT_EVENT_NAMES.giveaways.active);
   const activeGiveaways = await getActiveGiveaways();
   const markupButtons = activeGiveaways.map((item: Giveaway) => {
     return [
@@ -51,7 +50,10 @@ getGiveawaysInfo.action(BOT_EVENT_NAMES.giveaways.active, async (ctx: any) => {
   });
   await ctx.sendMessage("Выбрать из активных конкурсов", {
     reply_markup: {
-      inline_keyboard: [...markupButtons],
+      inline_keyboard: [
+        ...markupButtons,
+        [Markup.button.callback(CMD_TEXT.back, BOT_EVENT_NAMES.back)],
+      ],
     },
   });
 });
@@ -63,22 +65,23 @@ getGiveawaysInfo.action(BOT_EVENT_NAMES.giveaways.all, async (ctx: any) => {
 });
 
 getGiveawaysInfo.action(BOT_EVENT_NAMES.giveaways.ended, async (ctx: any) => {
-  // ctx.scene.state.giveaways = "ended";
-  // console.log(BOT_EVENT_NAMES.giveaways.ended);
-  // ctx.reply("Завершенные конкурсы");
-  console.log(BOT_EVENT_NAMES.giveaways.active);
   const activeGiveaways = await getEndedGiveaways();
   const markupButtons = activeGiveaways.map((item: Giveaway) => {
-    return [Markup.button.callback(item.title, `giveaway_ended ${item.id}`)];
+    return [
+      Markup.button.callback(item.title, `giveaway_ended ${item.messageId}`),
+    ];
   });
   await ctx.sendMessage("Выбрать из завершенных конкурсов", {
     reply_markup: {
-      inline_keyboard: [...markupButtons],
+      inline_keyboard: [
+        ...markupButtons,
+        [Markup.button.callback(CMD_TEXT.back, BOT_EVENT_NAMES.back)],
+      ],
     },
   });
 });
 
-// обработка нажатия кнопки выбора конкурса
+// обработка нажатия кнопки выбора АКТИВНОГО конкурса
 getGiveawaysInfo.action(/giveaway_active (.+)/, async (ctx: any) => {
   const giveawayId = ctx.match[1];
   ctx.scene.state.giveaway = giveawayId;
@@ -86,8 +89,14 @@ getGiveawaysInfo.action(/giveaway_active (.+)/, async (ctx: any) => {
   const giveaway = await findGiveaway(giveawayId);
   console.log("giveaway", giveaway);
   await ctx.reply(
-    "Выбрать действие: ",
+    `Выбрать действие над ${giveaway[0].title}:`,
     Markup.inlineKeyboard([
+      [
+        Markup.button.callback(
+          CMD_TEXT.giveawaysTexts.actions.info,
+          BOT_EVENT_NAMES.giveaways.actions.info
+        ),
+      ],
       [
         Markup.button.callback(
           CMD_TEXT.giveawaysTexts.actions.users,
@@ -106,8 +115,25 @@ getGiveawaysInfo.action(/giveaway_active (.+)/, async (ctx: any) => {
           BOT_EVENT_NAMES.giveaways.actions.close
         ),
       ],
-      [Markup.button.callback(CMD_TEXT.back, BOT_EVENT_NAMES.cancel)],
+      [Markup.button.callback(CMD_TEXT.back, BOT_EVENT_NAMES.back)],
     ])
+  );
+});
+
+// обработка нажатия кнопки выбора ЗАВЕРШЕННОГО конкурса
+getGiveawaysInfo.action(/giveaway_ended (.+)/, async (ctx: any) => {
+  const giveawayId = ctx.match[1];
+  const giveaway = await findGiveaway(giveawayId);
+  console.log("giveaway[0]", giveaway[0]);
+  const winner =
+    typeof giveaway[0].winner === "string"
+      ? giveaway[0].winner
+      : giveaway[0].winner.username;
+  await ctx.sendMessage(
+    `<b>Конкурс:</b> ${giveaway[0].title}\n<b>Дата завершения:</b> ${giveaway[0].endDate}\n<b>Победитель:</b> ${winner}`,
+    {
+      parse_mode: "HTML",
+    }
   );
 });
 
@@ -127,23 +153,94 @@ getGiveawaysInfo.action(BOT_EVENT_NAMES.giveaways.actions.finally, (ctx) => {
 getGiveawaysInfo.action(BOT_EVENT_NAMES.yes, async (ctx) => {
   const info: any[] = await getGiveawaInfo(ctx.scene.state.giveaway);
   const membersCount = info[0].memberInfo.length;
+  let member = null;
 
   if (membersCount === 0) {
     await ctx.reply("Никто не участвует в конкурсе");
   } else if (membersCount === 1) {
-    await ctx.reply(`Победитель: ${info[0].memberInfo[0].username}`);
+    member = info[0].memberInfo[0];
+    await ctx.reply(`Победитель: ${member.username}`);
   } else {
-    const number: number = getRandomInt(0, membersCount);
-    await ctx.reply(`Победитель: ${info[0].memberInfo[number].username}`);
+    const num: number = getRandomInt(0, membersCount);
+    member = info[0].memberInfo[num];
+    await ctx.reply(`Победитель: ${member.username}`);
   }
-  await updateGiveaway(ctx.scene.state.giveaway);
+  const winner = member ? member : "Никто не участвовал в конкурсе";
+  await updateGiveaway(ctx.scene.state.giveaway, {
+    isActive: false,
+    winner,
+  });
   await ctx.reply(`Конкурс "${info[0].title}" завершен`);
   return ctx.scene.leave();
 });
 
-// BOT_EVENT_NAMES.giveaways.actions.users
+getGiveawaysInfo.action(BOT_EVENT_NAMES.giveaways.actions.info, async (ctx) => {
+  const info: any[] = await getGiveawaInfo(ctx.scene.state.giveaway);
+  ctx.reply(
+    `<b>Конкурс:</b> ${info[0].title}\n<b>Предполагаемая дата завершения:</b> ${info[0].dateTo}\n<b>Количество участников:</b> ${info[0].memberInfo.length}`,
+    {
+      parse_mode: "HTML",
+      reply_markup: {
+        inline_keyboard: [
+          [Markup.button.callback(CMD_TEXT.back, BOT_EVENT_NAMES.back)],
+        ],
+      },
+    }
+  );
+});
 
-getGiveawaysInfo.action(BOT_EVENT_NAMES.cancel, (ctx) => {
-  ctx.reply("Отмена");
+getGiveawaysInfo.action(
+  BOT_EVENT_NAMES.giveaways.actions.users,
+  async (ctx) => {
+    const info: any[] = await getGiveawaInfo(ctx.scene.state.giveaway);
+    ctx.reply(
+      `<b>Конкурс:</b> ${info[0].title}\n<b>Участники:</b> ${info[0].memberInfo
+        .map((member) => member.username)
+        .join("\n")}`,
+      {
+        parse_mode: "HTML",
+        reply_markup: {
+          inline_keyboard: [
+            [Markup.button.callback(CMD_TEXT.back, BOT_EVENT_NAMES.back)],
+          ],
+        },
+      }
+    );
+  }
+);
+
+// завершить конкурс досрочно
+getGiveawaysInfo.action(
+  BOT_EVENT_NAMES.giveaways.actions.close,
+  async (ctx) => {
+    ctx.reply("Завершить конкурс досрочно?", {
+      reply_markup: {
+        inline_keyboard: [
+          [Markup.button.callback(CMD_TEXT.yes, BOT_EVENT_NAMES.yes_force)],
+          [Markup.button.callback(CMD_TEXT.no, BOT_EVENT_NAMES.back)],
+        ],
+      },
+    });
+  }
+);
+
+// ДА, завершить досрочно
+getGiveawaysInfo.action(BOT_EVENT_NAMES.yes_force, async (ctx) => {
+  await updateGiveaway(ctx.scene.state.giveaway, {
+    isActive: false,
+    winner: "Досрочное завершение",
+  });
+  await ctx.reply(`Конкурс "${ctx.scene.state.giveaway}" завершен`);
+  return ctx.scene.leave();
+});
+
+getGiveawaysInfo.action(BOT_EVENT_NAMES.back, async (ctx) => {
+  console.log("BOT_EVENT_NAMES.back");
+  await ctx.deleteMessage(ctx.update.callback_query.message.message_id);
+});
+
+getGiveawaysInfo.action(BOT_EVENT_NAMES.cancel, async (ctx) => {
+  await ctx.deleteMessage(ctx.update.callback_query.message.message_id);
+  ctx.reply(CMD_TEXT.cansel3);
   return ctx.scene.leave();
 });
--- src/scenes/giveawayCallback.ts
+++ src/scenes/giveawayCallback.ts
@@ -18,66 +18,22 @@ export const giveAwayCallbackScene: any = new Scenes.BaseScene(
 giveAwayCallbackScene.enter(async (ctx: any) => {
   const msgID = ctx.match[1];
   const member = await ctx.getChatMember(ctx.from.id);
+  // найти текущий конкурс
+  const item = await findGiveaway(msgID);
 
   // есть ли текущий пользователь в списке участников в текущем конкурсе
   const hasMember = await getGiveawayItemInfo(msgID, member.user.id);
   console.log("hasMember", hasMember);
-  if (hasMember.length === 0) {
-    const newMember = {
-      id: member.user.id,
-      status: member.status,
-      first_name: member.user.first_name,
-      username: member.user.username,
-    };
-    // обновить список участников для текущего конкурса
-    await updateGiveawayMemberInfo(msgID, newMember);
-    await ctx.answerCbQuery("Участвую", {
-      show_alert: true,
-    });
-  } else {
-    await ctx.answerCbQuery("Уже участвуете", {
-      show_alert: true,
-    });
-  }
-
-  const dbMember = await getGiveawayItemMemberInfo(
-    msgID,
-    String(member.user.id)
-  );
-  // console.log("dbMember", dbMember);
-  // console.log("member", member);
-  // найти текущий конкурс
-  const item = await findGiveaway(msgID);
-  // найти количество участников
-  const count = item[0].memberInfo.length;
-  // console.log("count", count);
-  // console.log("item[0].type", item[0].type);
-
-  const humanDate = getDateTime();
-  if (dbMember) {
-    const timeComment = Format.quote(`(${count})Участвуют на ${humanDate}`);
-    const endComment = Format.quote(`Завершение ${item[0].dateTo}`);
-    const newMessage = `${item[0].description}\n${timeComment}\n${endComment}`;
-    const options = {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            Markup.button.callback(
-              "Участвовать",
-              `${BOT_EVENT_NAMES.participation} ${msgID}`
-            ),
-          ],
-        ],
-      },
-    };
+  if (item[0].isActive === false) {
+    const newMessage = "Конкурс завершен";
     if (item[0].type === "text") {
       try {
         await ctx.telegram.editMessageText(
           process.env.CHAT_ID,
           ctx.callbackQuery.message.message_id,
           null,
-          newMessage,
-          options
+          newMessage
+          // options
         );
       } catch (error) {
         console.log(error);
@@ -88,14 +44,98 @@ giveAwayCallbackScene.enter(async (ctx: any) => {
           process.env.CHAT_ID,
           ctx.callbackQuery.message.message_id,
           null,
-          newMessage,
-          options
+          newMessage
+          // options
         );
       } catch (error) {
         console.log(error);
       }
     }
   } else {
-    console.log("!!!dbMember");
+    if (hasMember.length === 0) {
+      const newMember = {
+        id: member.user.id,
+        status: member.status,
+        first_name: member.user.first_name,
+        username: member.user.username,
+      };
+      // обновить список участников для текущего конкурса
+      await updateGiveawayMemberInfo(msgID, newMember);
+      await ctx.answerCbQuery("Участвую", {
+        show_alert: true,
+      });
+    } else {
+      await ctx.answerCbQuery("Уже участвуете", {
+        show_alert: true,
+      });
+    }
+
+    const dbMember = await getGiveawayItemMemberInfo(
+      msgID,
+      String(member.user.id)
+    );
+
+    // найти количество участников
+    const count = item[0].memberInfo.length;
+
+    const humanDate = getDateTime();
+    if (dbMember) {
+      const timeComment = Format.quote(`(${count})Участвуют на ${humanDate}`);
+      const endComment = Format.quote(`Завершение ${item[0].dateTo}`);
+      const newMessage = Format.fmt([
+        `${item[0].description}\n`,
+        `${timeComment}\n`,
+        `${endComment}`,
+      ]);
+      // ctx.reply(
+      //   Format.fmt(
+      //     [
+      //       Format.italic("Введите описание c фото или без."),
+      //       "Это описание будет отображаться при публикации",
+      //       Format.bold("Пример:"),
+      //     ],
+      //     " "
+      //   )
+      // );
+      const options = {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              Markup.button.callback(
+                "Участвовать",
+                `${BOT_EVENT_NAMES.participation} ${msgID}`
+              ),
+            ],
+          ],
+        },
+      };
+      if (item[0].type === "text") {
+        try {
+          await ctx.telegram.editMessageText(
+            process.env.CHAT_ID,
+            ctx.callbackQuery.message.message_id,
+            null,
+            newMessage,
+            options
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      } else if (item[0].type === "photo") {
+        try {
+          await ctx.telegram.editMessageCaption(
+            process.env.CHAT_ID,
+            ctx.callbackQuery.message.message_id,
+            null,
+            newMessage,
+            options
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    } else {
+      console.log("!!!dbMember");
+    }
   }
 });
